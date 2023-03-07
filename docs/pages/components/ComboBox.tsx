2726cf6698565eaeed7d69b58f473969eb9bde6c
Olivier Tassinari <olivier.tassinari@gmail.com>
2023-03-07 14:10:59 +0100

    Propose different ways to style (#24)

--- docs/pages/components/ComboBox.tsx
+++ docs/pages/components/ComboBox.tsx
@@ -1,4 +1,14 @@
 import * as React from 'react';
+// You have multiple options to add styles:
+// emotion
+//   import styled from '@emotion/styled';
+// CSS modules
+//   import styles from './style.module.css'
+// JSS
+//   import { styled } from '@material-ui/styles';
+// styled-components
+//   import styled, { css } from 'styled-components';
+// Inline style
 
 export default function ComboBox() {
   return <div />;
